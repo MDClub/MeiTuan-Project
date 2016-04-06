3f67d83f69778955faeab644c36ea6440c4e90fc
QMSAE86 <501710270@qq.com>
2016-04-06 13:47:11 +0800

    project
    project

---
+++ FaceRecognition_by_myself/FaceRecognition_by_myself/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace FaceRecognition_by_myself
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Timer_textBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.LabelEdit = true;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(13, 535);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(765, 95);
+            this.listView1.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.listView1, "这里储存训练过的人脸");
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.listView1_AfterLabelEdit);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(64, 64);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(674, 295);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(83, 29);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Open";
+            this.toolTip1.SetToolTip(this.startButton, "开始或者停止");
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(674, 239);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Learn";
+            this.toolTip1.SetToolTip(this.button2, "训练\r\n如果是pic模式，从图里读取人脸\r\n如果是webcam模式，直接保存识别出的人脸");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 10;
+            this.toolTip1.AutoPopDelay = 10000;
+            this.toolTip1.InitialDelay = 10;
+            this.toolTip1.ReshowDelay = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(678, 74);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(108, 19);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "by picture";
+            this.toolTip1.SetToolTip(this.radioButton1, "用图片来玩");
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(678, 113);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(100, 19);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.Text = "by WebCam";
+            this.toolTip1.SetToolTip(this.radioButton2, "用摄像头玩");
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // imageBox
+            // 
+            this.imageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox.Location = new System.Drawing.Point(13, 31);
+            this.imageBox.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(640, 480);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox.TabIndex = 7;
+            this.imageBox.TabStop = false;
+            this.toolTip1.SetToolTip(this.imageBox, "监控器");
+            this.imageBox.Paint += new System.Windows.Forms.PaintEventHandler(this.imageBox_Paint);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(674, 348);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 19);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "AUTO STOP";
+            this.toolTip1.SetToolTip(this.checkBox1, "识别到人脸后自动停下来");
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(678, 149);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(92, 19);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.Text = "by video";
+            this.toolTip1.SetToolTip(this.radioButton3, "播放视频");
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Camera";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 516);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Learned faces(0)";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            // 
+            // Timer_textBox
+            // 
+            this.Timer_textBox.Location = new System.Drawing.Point(678, 192);
+            this.Timer_textBox.MaxLength = 4;
+            this.Timer_textBox.Name = "Timer_textBox";
+            this.Timer_textBox.Size = new System.Drawing.Size(79, 25);
+            this.Timer_textBox.TabIndex = 10;
+            this.Timer_textBox.Text = "80";
+            this.toolTip1.SetToolTip(this.Timer_textBox, "人脸检测时间间隔");
+            this.Timer_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Timer_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(788, 637);
+            this.Controls.Add(this.Timer_textBox);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.listView1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox Timer_textBox;
+    }
+}
+
---
+++ FaceRecognition_by_myself/FaceRecognition_by_myself/Form1.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+namespace  FaceRecognition_by_myself
+{
+    public partial class Form1 : Form
+    {
+        #region variables
+        // webcam
+        Capture webcam;
+        Capture cptvideo;
+        int VideoFps;//视频帧率
+        bool cameraInUse = false;
+        bool captureInProgress = false;
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        [DllImport("FDDll.dll", EntryPoint = "Mytest", CallingConvention = CallingConvention.Cdecl)]
+        unsafe public static extern int* Mytest(int whidth, int height, IntPtr pImg);
+
+        MCvFont font = new MCvFont(
+            Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX,
+            1.2,
+            1.2
+            );
+
+        Image<Bgr, byte> current;
+        Image<Gray, byte> gray = null;
+        Image<Bgr, byte> imgshow = null;
+
+
+        List<Image<Gray, byte>> learnedFaces = new List<Image<Gray, byte>>();
+        List<string> learnedPeople = new List<string>();
+
+        unsafe int* pf = null;
+        #endregion
+
+        #region initiates
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void initHaar(ref HaarCascade h, string path)
+        //{
+        //    h = new HaarCascade(path);
+        //}
+
+        // try load early trained faces
+        private void loadBackupImages(List<Image<Gray, byte>> list)
+        {
+          
+        }
+        #endregion
+
+        #region private helpers
+        // initiate webcam
+        private bool initiateWebCam(int index)
+        {
+            if (webcam != null)
+                return false;
+
+            try
+            {
+                webcam = new Capture(index);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("WEBCAM:" + ex.Message);
+                return false;
+            }
+        }
+
+        // process camera
+        private void processCamera(object sender, EventArgs e)
+        {
+            current = webcam.QueryFrame();
+            // flip because my webcam is MIRRORED
+            current = current.Flip(Emgu.CV.CvEnum.FLIP.HORIZONTAL);
+            imageBox.Image = current.Bitmap;
+        }
+
+        #endregion
+
+        private void  ProcessFrame(object sender, EventArgs e)
+        {
+            current =  cptvideo.QueryFrame();           
+            //为使播放顺畅，添加以下延时
+            System.Threading.Thread.Sleep((int)(1000.0 / VideoFps) - 20);
+            if (current != null)
+            {
+                imageBox.Image = current.Bitmap;
+            }
+                       
+        }
+
+        #region analyze
+       
+
+        private void markFaces(Image<Bgr, byte> img)
+        {
+            if (img == null)
+                return;
+            Bitmap image = img.ToBitmap();
+            IntPtr pImg = IntPtr.Zero;
+            //System.Runtime.InteropServices.Marshal.StructureToPtr(image, pImg, false);
+
+
+
+            //锁定Bitmap数据
+            BitmapData bmpData = image.LockBits(
+            new Rectangle(0, 0, image.Width, image.Height),
+            System.Drawing.Imaging.ImageLockMode.ReadWrite, image.PixelFormat);
+
+            IntPtr ptrImg = bmpData.Scan0;
+            unsafe
+            {
+                //int* pf = null;
+                DateTime beforDT = System.DateTime.Now;
+                pf = Mytest(image.Width, image.Height, ptrImg);
+                DateTime afterDT = System.DateTime.Now;
+                TimeSpan ts = afterDT.Subtract(beforDT);
+                Console.WriteLine("DateTime总共花费{0}ms.", ts.TotalMilliseconds);
+
+                foundPeople.Clear();
+                if (pf == null||*pf == 0)
+                    return;
+
+
+                if (checkBox1.Checked)
+                    // IF FOUND, AUTO STOP
+                    if (*pf > 0)
+                        startButton_Click(null, null);
+
+                for (int i = 0; i < *pf; i++)
+                {
+                    //MCvAvgComp face;
+                    //face.rect.X
+                    Rectangle facerect = new Rectangle(*(pf + 4 * i + 1), *(pf + 4 * i + 2), *(pf + 4 * i + 3), *(pf + 4 * i + 4));
+
+                    current.Draw(facerect, new Bgr(Color.Lime), 2);
+
+                    //MCvAvgComp face = faces[i];
+                    Image<Gray, byte> result = current
+                        .Copy(facerect)
+                        .Convert<Gray, byte>()
+                        .Resize(64, 64, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+                    //current.Draw(face.rect, new Bgr(Color.Lime), 2);
+
+                    // RECOGNIZE!
+                    if (learnedFaces.Count > 0)
+                    {
+                        //TermCriteria 
+                        //for face recognition
+                        MCvTermCriteria tc = new MCvTermCriteria(learnedFaces.Count, 0.001);
+
+                        //Eigen face recognizer
+                        EigenObjectRecognizer recognizer = new EigenObjectRecognizer(
+                           learnedFaces.ToArray(),
+                           learnedPeople.ToArray(),
+                           3000,
+                           ref tc);
+                        string name = "";
+                        if (recognizer.Recognize(result) != null)
+                            name = recognizer.Recognize(result).Label;//改过
+                        foundPeople[name] = facerect;
+                        //current.Draw(name,ref font,new Point(face.rect.X - 2, face.rect.Y - 2),new Bgr(Color.Blue));
+                    }
+
+                }
+                imageBox.Image = current.Bitmap;
+            }
+        }
+
+        Dictionary<string, Rectangle> foundPeople = new Dictionary<string, Rectangle>();
+
+        float xfactor;
+        float yfactor;
+
+        private void imageBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (foundPeople.Count > 0&&current!=null)
+            {
+                // 缩放
+                xfactor = (float)imageBox.Width / (float)current.Bitmap.Width;
+                yfactor = (float)imageBox.Height / (float)current.Bitmap.Height;
+
+                foreach (string name in foundPeople.Keys)
+                {
+                    e.Graphics.DrawString(
+                        name,
+                        new Font("verdana", 12),//this.Font,
+                        Brushes.Red,
+                        foundPeople[name].X * xfactor,
+                        foundPeople[name].Y * yfactor);
+                    e.Graphics.DrawRectangle(new Pen(Color.Red), 
+                        foundPeople[name].X * xfactor,
+                        foundPeople[name].Y * yfactor,
+                        foundPeople[name].Width * xfactor,
+                        foundPeople[name].Height * yfactor);
+                }
+            }
+        }
+
+        #endregion
+
+        #region action
+        // process faces (do this while idle, that is every moment)
+        private void processFaces(object sender, EventArgs arg)
+        {
+            //foundFaces = getFaces(current, faceHaar);
+            //markFaces(foundFaces);
+            markFaces(current);
+        }
+
+        //start/stop
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            this.timer1.Interval  = Convert.ToInt16(Timer_textBox.Text);
+            if (radioButton1.Checked)
+            {
+                //pic mode
+
+                openFileDialog.Filter = "所有文件|*.*|JPG文件|*.jpg|PNG文件|*.png|BMP文件|*.bmp|JPEG文件|*.jpeg";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //current = new Image<Bgr, byte>("1.jpg");
+                    current = new Image<Bgr, byte>(openFileDialog.FileName);
+                    current = current.Flip(Emgu.CV.CvEnum.FLIP.HORIZONTAL);
+                    imageBox.Image = current.Bitmap;
+                    checkBox1.Checked = false;
+                    markFaces(current);
+                }
+                //cptvideo = null;
+            }
+            else if (radioButton2.Checked)
+            {
+
+
+                // webcam ok
+                if (cameraInUse)
+                {
+                    Application.Idle -= new EventHandler(processCamera);
+                    timer1.Tick -= new EventHandler(processFaces);
+                }
+                else
+                {
+                    if (webcam == null)
+                        return;
+                    Application.Idle += new EventHandler(processCamera);
+                    timer1.Tick += new EventHandler(processFaces);
+                }
+                cameraInUse = !cameraInUse;
+
+                radioButton1.Enabled = !cameraInUse;
+                radioButton3.Enabled = !cameraInUse;
+                //cptvideo = null;
+            }
+            else
+            {
+                if (cptvideo == null)
+                {
+                    try
+                    {
+                        openFileDialog.Filter = "所有文件|*.*|AVI文件|*.avi|RMVB文件|*.rmvb|WMV文件|*.wmv|MKV文件|*.mkv|MP4文件|*.mp4";
+                        if (openFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            cptvideo = new Capture(openFileDialog.FileName);
+                            VideoFps = (int)CvInvoke.cvGetCaptureProperty(cptvideo, Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS);
+                        }
+                        
+                    }
+                    catch (NullReferenceException excpt)
+                    {
+                        MessageBox.Show(excpt.Message);
+                    }
+                }
+                if (cptvideo != null)
+                {
+                    if (captureInProgress)
+                    {  //stop the capture
+                        Application.Idle -= new EventHandler(ProcessFrame);
+                        timer1.Tick -= new EventHandler(processFaces);
+                        startButton.Text = "Play";
+                    }
+                    else
+                    {
+                        //start the capture
+                        Application.Idle += new EventHandler(ProcessFrame);
+                        timer1.Tick += new EventHandler(processFaces);
+                        startButton.Text = "Pause";
+                    }
+
+                    captureInProgress = !captureInProgress;
+                }
+                radioButton1.Enabled = !captureInProgress;
+                radioButton2.Enabled = !captureInProgress;
+            }
+        }
+
+        // first load & run
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // initiates with old backup files
+            loadBackupImages(learnedFaces);
+            //webcam mode
+            if (!initiateWebCam(0))
+            {
+                MessageBox.Show("Failed to initiate webcam\r\nAre you sure you have one?");
+                return;
+            }            
+            
+        }
+
+        // webcam mode
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                startButton.Text = "Strt/Stp";
+                toolTip1.SetToolTip(startButton, "启动/停止摄像头");
+                current = null;
+                imageBox.Image = null;
+                cptvideo = null;
+                foundPeople.Clear();
+            }
+        }
+
+        //pic mode
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                startButton.Text = "Open";
+                toolTip1.SetToolTip(startButton, "开始识别脑壳");
+                current = null;
+                imageBox.Image = null;
+                cptvideo = null;
+                foundPeople.Clear();
+            }
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                startButton.Text = "Open";
+                toolTip1.SetToolTip(startButton, "开始识别人脸");
+                current = null;
+                imageBox.Image = null;
+                foundPeople.Clear();
+            }
+        }
+
+        // learn people
+        private void button2_Click(object sender, EventArgs e)
+        {
+            unsafe
+            {
+
+                if (pf == null || *pf == 0)
+                    return;
+
+                string name = "";
+
+                for (int i = 0; i < *pf; i++)
+                {
+                    Rectangle facerect = new Rectangle(*(pf + 4 * i + 1), *(pf + 4 * i + 2), *(pf + 4 * i + 3), *(pf + 4 * i + 4));
+                    name = "Unknown" + learnedFaces.Count;
+                    imgshow = current
+                        .Copy(facerect)
+                        .Resize(64, 64, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                    gray = current
+                        .Copy(facerect)
+                        .Convert<Gray, byte>()
+                        .Resize(64, 64, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+                    learnedFaces.Add(gray);
+                    learnedPeople.Add(name);
+
+                    imageList1.Images.Add(imgshow.Bitmap);
+                    listView1.Items.Add(name);
+                    listView1.Items[listView1.Items.Count - 1].ImageIndex =
+                        imageList1.Images.Count - 1;
+                }
+            }
+        }
+
+        // udpate count of learned
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Text = string.Format("Learned faces:{0}", listView1.Items.Count);
+        }
+
+        // ...
+        private void listView1_AfterLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.Label))
+                e.CancelEdit = true;
+            else
+                // Check to ensure no duplicated names
+                if (listView1.Items.ContainsKey(e.Label))
+                    e.CancelEdit = true;
+                else
+                    learnedPeople[e.Item] = e.Label;
+        }
+        #endregion
+
+        private void Timer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char result = e.KeyChar;
+            if (char.IsDigit(result) || result == 8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+
+
+    }
+}
